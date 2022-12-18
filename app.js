97629ec9bfbe4e3b608c62124683abef2540f875
mabdullah <mabdullah2037@gmail.com>
2022-12-18 12:31:40 +0500

    first assignment completed

---
+++ app.js
@@ -0,0 +1,116 @@
+var a = {
+  mobiles: {
+    samsung: {
+      a30: {
+        name: "a30",
+        ram: "4gb",
+        rom: "64gb",
+        camera: "50px",
+        price: 2000,
+      },
+      note10: {
+        name: "note10",
+        ram: "6gb",
+        rom: "128gb",
+        camera: "50px",
+        price: 4000,
+      },
+      s10: {
+        name: "s10",
+        ram: "3gb",
+        rom: "128gb",
+        camera: "10px",
+        price: 5000,
+      },
+    },
+    iphone: {
+      iphone4: {
+        name: "iphone4",
+        ram: "4gb",
+        rom: "64gb",
+        camera: "50px",
+        price: 10000,
+      },
+      iphone4s: {
+        name: "iphone4s",
+        ram: "4gb",
+        rom: "64gb",
+        camera: "50px",
+        price: 20000,
+      },
+      iphone5: {
+        name: "iphone5",
+        ram: "4gb",
+        rom: "64gb",
+        camera: "50px",
+        price: 30000,
+      },
+      iphone6: {
+        name: "iphone6",
+        ram: "4gb",
+        rom: "64gb",
+        camera: "50px",
+        price: 30000,
+      },
+    },
+    xiomi: {
+      redminote10: {
+        name: "redminote10",
+        ram: "4gb",
+        rom: "64gb",
+        camera: "50px",
+        price: 10000,
+      },
+      redminote11: {
+        name: "redminote11",
+        ram: "4gb",
+        rom: "64gb",
+        camera: "50px",
+        price: 10000,
+      },
+      redmi10: {
+        name: "redmi10",
+        ram: "4gb",
+        rom: "64gb",
+        camera: "50px",
+        price: 10000,
+      },
+      redmi10pro: {
+        name: "redmi10pro",
+        ram: "4gb",
+        rom: "64gb",
+        camera: "50px",
+        price: 10000,
+      },
+    },
+  },
+};
+
+
+
+
+
+let brand = document.getElementById("brand");
+let phone = document.getElementById("phone");
+let button = document.getElementById("find");
+
+
+let brandName = Object.keys(a.mobiles);
+brandName.map((item)=>{
+    brand.innerHTML = brand.innerHTML + `<option>${item}</option>`
+});
+
+brand.addEventListener("change" , ()=>{
+    phone.innerHTML = ""
+    let phoneName = Object.keys(a.mobiles[brand.value]);
+    // console.log(phoneName);
+    phoneName.map((item)=>{
+        phone.innerHTML = phone.innerHTML + `<option>${item}</option>`
+    });
+})
+
+
+button.addEventListener("click" , ()=>{
+    brand.value === "Select brand Name" ? console.log("please select brand Name"):
+    console.log(a.mobiles[brand.value][phone.value]);
+})
